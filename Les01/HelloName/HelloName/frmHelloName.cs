6af86616a55f88bee0785b513294528c50618488
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2025-11-13 11:53:37 +0100

    13111153

---
+++ Les01/HelloName/HelloName/frmHelloName.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace HelloName
+{
+    public partial class frmHelloName : Form
+    {
+        public frmHelloName()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGreet_Click(object sender, EventArgs e)
+        {
+            // naam uit tekstvak halen en opslaan in variabele
+            string name = txtName.Text;
+
+            // zin opbouwen in variabele
+            string greeting = "Hallo " + name;            
+
+            // greeting in ander tekstvak plaatsen
+            txtResult.Text = greeting;            
+
+        }
+    }
+}
