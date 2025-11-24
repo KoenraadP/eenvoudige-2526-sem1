1ccde99060c4e1c2d4736b990061ff65bb042608
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2025-11-24 13:47:36 +0100

    2411251347

---
+++ Les04/distance/distance/frmDistance.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace distance
+{
+    partial class frmDistance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAvgSpeed = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDuration = new System.Windows.Forms.TextBox();
+            this.btnDistance = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gemiddelde snelheid in km/u";
+            // 
+            // txtAvgSpeed
+            // 
+            this.txtAvgSpeed.Location = new System.Drawing.Point(163, 10);
+            this.txtAvgSpeed.Name = "txtAvgSpeed";
+            this.txtAvgSpeed.Size = new System.Drawing.Size(42, 20);
+            this.txtAvgSpeed.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tijd gereden";
+            // 
+            // txtDuration
+            // 
+            this.txtDuration.Location = new System.Drawing.Point(163, 43);
+            this.txtDuration.Name = "txtDuration";
+            this.txtDuration.Size = new System.Drawing.Size(42, 20);
+            this.txtDuration.TabIndex = 3;
+            // 
+            // btnDistance
+            // 
+            this.btnDistance.Location = new System.Drawing.Point(16, 80);
+            this.btnDistance.Name = "btnDistance";
+            this.btnDistance.Size = new System.Drawing.Size(189, 23);
+            this.btnDistance.TabIndex = 4;
+            this.btnDistance.Text = "Bereken afstand";
+            this.btnDistance.UseVisualStyleBackColor = true;
+            this.btnDistance.Click += new System.EventHandler(this.BtnDistance_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(16, 110);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(189, 85);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // frmDistance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(221, 207);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnDistance);
+            this.Controls.Add(this.txtDuration);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAvgSpeed);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDistance";
+            this.Text = "Distance";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAvgSpeed;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDuration;
+        private System.Windows.Forms.Button btnDistance;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ Les04/distance/distance/frmDistance.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace distance
+{
+    public partial class frmDistance : Form
+    {
+
+        #region globals
+
+        double avgSpeed;
+        double duration;
+
+        #endregion
+
+        public frmDistance()
+        {
+            InitializeComponent();
+        }
+
+        #region methods
+
+        // eerste methode: 2 textboxes lezen
+        // en inhoud omzetten naar double waarden
+        // deze dan ook opslaan in globale variabelen
+        private void ReadInputs(TextBox box1, TextBox box2)
+        {
+            avgSpeed = Convert.ToDouble(box1.Text);
+            duration = Convert.ToDouble(box2.Text);
+        }
+
+        // methode om de twee cijfers met elkaar te vermenigvuldigen
+        // resultaat is opnieuw hetzelfde type als de invoer cijfers
+        // in dit geval dus 'double'
+        private double CalculateDistance(double speed, double timeInMinutes)
+        {
+            double result = (speed/60) * timeInMinutes;
+            return result;
+        }
+
+        // methode die een uitvoer 'zin' maakt met het antwoord hierin
+        private string Output(double distanceDriven)
+        {
+            string output = "Na " + duration + " minuten rijden "
+                            + "aan een gemiddelde snelheid van " + avgSpeed
+                            + " km per uur heb je " + distanceDriven
+                            + " km gereden.";
+            return output;
+        }
+
+        #endregion
+
+        private void BtnDistance_Click(object sender, EventArgs e)
+        {
+            // gemaakte methodes aanroepen
+            ReadInputs(txtAvgSpeed, txtDuration);
+            double distance = CalculateDistance(avgSpeed, duration);
+            distance = Math.Round(distance, 2);
+            txtOutput.Text = Output(distance);
+        }
+    }
+}
