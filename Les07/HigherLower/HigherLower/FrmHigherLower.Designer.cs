57ea60112b5bf386f2f805255b2327b6978b352a
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2025-12-04 09:21:38 +0100

    0412250921

---
+++ Les07/HigherLower/HigherLower/FrmHigherLower.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace HigherLower
+{
+    partial class FrmHigherLower
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(13, 13);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(100, 20);
+            this.txtGuess.TabIndex = 0;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(13, 40);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(100, 23);
+            this.btnGuess.TabIndex = 1;
+            this.btnGuess.Text = "Cijfer raden";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            // 
+            // FrmHigherLower
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(132, 75);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtGuess);
+            this.Name = "FrmHigherLower";
+            this.Text = "Hoger Lager";
+            this.Load += new System.EventHandler(this.FrmHigherLower_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnGuess;
+    }
+}
+
---
+++ Les07/HigherLower/HigherLower/FrmHigherLower.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HigherLower
+{
+    public partial class FrmHigherLower : Form
+    {
+        public FrmHigherLower()
+        {
+            InitializeComponent();
+        }
+
+        // Load wordt uitgevoerd bij starten van programma
+        private void FrmHigherLower_Load(object sender, EventArgs e)
+        {
+            // een Random variabele maken 
+            // om te kunnen een random cijfer genereren
+            Random rng = new Random();
+            // random cijfer kiezen vanaf 1 tot en met 5
+            // let op: tweede parameter is de bovengrens maar is EXCLUSIEF dit cijfer
+            int randomNumber = rng.Next(1,6);
+            // random cijfer tonen in output venster
+            Debug.WriteLine(randomNumber.ToString());
+        }
+    }
+}
