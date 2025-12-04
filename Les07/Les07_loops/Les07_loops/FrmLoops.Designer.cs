06a4e1db7501f3352c2414a4fb2608fe5f845424
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2025-12-04 10:14:00 +0100

    1013

---
+++ Les07/Les07_loops/Les07_loops/FrmLoops.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Les07_loops
+{
+    partial class FrmLoops
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumbers = new System.Windows.Forms.TextBox();
+            this.btnShowNumbers1 = new System.Windows.Forms.Button();
+            this.btnShowNumbers2 = new System.Windows.Forms.Button();
+            this.btnShowNumbers3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumbers
+            // 
+            this.txtNumbers.Location = new System.Drawing.Point(13, 99);
+            this.txtNumbers.Multiline = true;
+            this.txtNumbers.Name = "txtNumbers";
+            this.txtNumbers.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtNumbers.Size = new System.Drawing.Size(100, 243);
+            this.txtNumbers.TabIndex = 0;
+            // 
+            // btnShowNumbers1
+            // 
+            this.btnShowNumbers1.Location = new System.Drawing.Point(13, 12);
+            this.btnShowNumbers1.Name = "btnShowNumbers1";
+            this.btnShowNumbers1.Size = new System.Drawing.Size(100, 23);
+            this.btnShowNumbers1.TabIndex = 1;
+            this.btnShowNumbers1.Text = "1 - 10";
+            this.btnShowNumbers1.UseVisualStyleBackColor = true;
+            this.btnShowNumbers1.Click += new System.EventHandler(this.BtnShowNumbers_Click);
+            // 
+            // btnShowNumbers2
+            // 
+            this.btnShowNumbers2.Location = new System.Drawing.Point(13, 41);
+            this.btnShowNumbers2.Name = "btnShowNumbers2";
+            this.btnShowNumbers2.Size = new System.Drawing.Size(100, 23);
+            this.btnShowNumbers2.TabIndex = 2;
+            this.btnShowNumbers2.Text = "1 - 100";
+            this.btnShowNumbers2.UseVisualStyleBackColor = true;
+            this.btnShowNumbers2.Click += new System.EventHandler(this.BtnShowNumbers2_Click);
+            // 
+            // btnShowNumbers3
+            // 
+            this.btnShowNumbers3.Location = new System.Drawing.Point(13, 70);
+            this.btnShowNumbers3.Name = "btnShowNumbers3";
+            this.btnShowNumbers3.Size = new System.Drawing.Size(100, 23);
+            this.btnShowNumbers3.TabIndex = 3;
+            this.btnShowNumbers3.Text = "1 - 1000";
+            this.btnShowNumbers3.UseVisualStyleBackColor = true;
+            this.btnShowNumbers3.Click += new System.EventHandler(this.BtnShowNumbers3_Click);
+            // 
+            // FrmLoops
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 356);
+            this.Controls.Add(this.btnShowNumbers3);
+            this.Controls.Add(this.btnShowNumbers2);
+            this.Controls.Add(this.btnShowNumbers1);
+            this.Controls.Add(this.txtNumbers);
+            this.Name = "FrmLoops";
+            this.Text = "Loops";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumbers;
+        private System.Windows.Forms.Button btnShowNumbers1;
+        private System.Windows.Forms.Button btnShowNumbers2;
+        private System.Windows.Forms.Button btnShowNumbers3;
+    }
+}
+
---
+++ Les07/Les07_loops/Les07_loops/FrmLoops.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Les07_loops
+{
+    public partial class FrmLoops : Form
+    {
+        public FrmLoops()
+        {
+            InitializeComponent();
+        }
+
+        // methode die een for loop gebruikt
+        private string ForLoopNumbers(int max)
+        {
+            // variabele voor return
+            string output = "";
+
+            // alle cijfers vanaf 1 tot en met 10 overlopen
+            for (int i = 1; i <= max; i++)
+            {
+                // huidig cijfer (i) toevoegen aan output string
+                output += i.ToString();
+                // 'nieuwe regel' toevoegen aan string
+                output += Environment.NewLine;
+                //output += "\r\n";
+            }
+
+            return output;
+        }
+
+        private void BtnShowNumbers_Click(object sender, EventArgs e)
+        {
+            string output = ForLoopNumbers(10);
+            // getrimde output in textbox plaatsen
+            // trim --> alle spaties/witregels voor en na de string verwijderen
+            txtNumbers.Text = output.Trim();
+        }
+
+        private void BtnShowNumbers2_Click(object sender, EventArgs e)
+        {
+            string output = ForLoopNumbers(100);
+            // getrimde output in textbox plaatsen
+            // trim --> alle spaties/witregels voor en na de string verwijderen
+            txtNumbers.Text = output.Trim();
+        }
+
+        private void BtnShowNumbers3_Click(object sender, EventArgs e)
+        {
+            string output = ForLoopNumbers(1000);
+            // getrimde output in textbox plaatsen
+            // trim --> alle spaties/witregels voor en na de string verwijderen
+            txtNumbers.Text = output.Trim();
+        }
+    }
+}
