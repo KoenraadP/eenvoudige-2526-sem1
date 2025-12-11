89f82052b60dd34af6e2061aa605e2cc34a3bcb7
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2025-12-11 10:19:36 +0100

    1112251020

---
+++ Les09/Arrays01/Arrays01/FrmArrays.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Arrays01
+{
+    partial class FrmArrays
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNumbers = new System.Windows.Forms.Button();
+            this.lbxData = new System.Windows.Forms.ListBox();
+            this.btnNames = new System.Windows.Forms.Button();
+            this.btnFetch = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnNumbers
+            // 
+            this.btnNumbers.Location = new System.Drawing.Point(13, 13);
+            this.btnNumbers.Name = "btnNumbers";
+            this.btnNumbers.Size = new System.Drawing.Size(120, 23);
+            this.btnNumbers.TabIndex = 0;
+            this.btnNumbers.Text = "Numbers";
+            this.btnNumbers.UseVisualStyleBackColor = true;
+            this.btnNumbers.Click += new System.EventHandler(this.BtnNumbers_Click);
+            // 
+            // lbxData
+            // 
+            this.lbxData.FormattingEnabled = true;
+            this.lbxData.Location = new System.Drawing.Point(13, 42);
+            this.lbxData.Name = "lbxData";
+            this.lbxData.Size = new System.Drawing.Size(120, 95);
+            this.lbxData.TabIndex = 1;
+            // 
+            // btnNames
+            // 
+            this.btnNames.Location = new System.Drawing.Point(13, 143);
+            this.btnNames.Name = "btnNames";
+            this.btnNames.Size = new System.Drawing.Size(120, 23);
+            this.btnNames.TabIndex = 2;
+            this.btnNames.Text = "Names";
+            this.btnNames.UseVisualStyleBackColor = true;
+            // 
+            // btnFetch
+            // 
+            this.btnFetch.Location = new System.Drawing.Point(13, 172);
+            this.btnFetch.Name = "btnFetch";
+            this.btnFetch.Size = new System.Drawing.Size(120, 23);
+            this.btnFetch.TabIndex = 3;
+            this.btnFetch.Text = "Fetch name";
+            this.btnFetch.UseVisualStyleBackColor = true;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(13, 202);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(120, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // FrmArrays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(148, 237);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnFetch);
+            this.Controls.Add(this.btnNames);
+            this.Controls.Add(this.lbxData);
+            this.Controls.Add(this.btnNumbers);
+            this.Name = "FrmArrays";
+            this.Text = "Arrays01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNumbers;
+        private System.Windows.Forms.ListBox lbxData;
+        private System.Windows.Forms.Button btnNames;
+        private System.Windows.Forms.Button btnFetch;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ Les09/Arrays01/Arrays01/FrmArrays.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arrays01
+{
+    public partial class FrmArrays : Form
+    {
+        public FrmArrays()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnNumbers_Click(object sender, EventArgs e)
+        {
+            // array aanmaken met daarin vijf cijfers
+            int[] numbers = { 20, 300, 15, 18, 20 };
+
+            // toon het derde cijfer in output
+            // Debug.WriteLine(numbers[2]);
+
+            // handmatig een per een aan listbox toevoegen
+            //lbxData.Items.Add(numbers[0]);
+            //lbxData.Items.Add(numbers[1]);
+            //lbxData.Items.Add(numbers[2]);
+
+            // met for loop
+            // de .Length is altijd het aantal elementen
+            // in de array, dus is altijd 1 hoger dan de laatste index
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                // huidig element op huidige index 
+                // toevoegen aan listbox
+                lbxData.Items.Add(numbers[i]);
+            }
+        }
+    }
+}
