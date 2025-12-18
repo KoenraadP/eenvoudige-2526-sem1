03c81f3e9bef38e028c5bace2afd816c398ab8ed
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2025-12-18 09:44:08 +0100

    181225944

--- Les11/Bestelling/Bestelling/FrmBestelling.cs
+++ Les11/Bestelling/Bestelling/FrmBestelling.cs
@@ -12,27 +12,55 @@ namespace Bestelling
 {
     public partial class FrmBestelling : Form
     {
+        // globale list aanmaken
+        List<string> orderItems = new List<string>();
+
         public FrmBestelling()
         {
             InitializeComponent();
         }
 
-        private void BtnAdd_Click(object sender, EventArgs e)
+        // methode om invoer uit txtItem
+        // toe te voegen aan list en listbox te updaten
+        private void AddItem()
         {
-            // list voor string items maken
-            List<string> orderItems = new List<string>();
-
             // invoer uit txtItem toevoegen aan list
             orderItems.Add(txtItem.Text);
+            // updateorder methode uitvoeren om in listbox te tonen
+            UpdateOrder();
+        }
+
+        // methode om listbox te updaten
+        private void UpdateOrder()
+        {
+            // listbox eerst weer leegmaken voor de update
+            lbxOrder.Items.Clear();
 
             // met foreach alle items uit list tonen in listbox
             foreach (var item in orderItems)
             {
                 lbxOrder.Items.Add(item);
             }
+        }
 
-            // input vak (txtItem) weer leeg maken voor volgende item
-            txtItem.Clear();
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            // controleren of textbox niet leeg is
+            // als dit OK is --> dan wel code uitvoeren, anders foutboodschap
+            if (txtItem.Text != "")
+            {
+                // add item methode uitvoeren om item toe te voegen
+                // aan zowel list als listbox
+                AddItem();
+
+                // input vak (txtItem) weer leeg maken voor volgende item
+                txtItem.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Geen item ingevoerd!", "Fout",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);    
+            }
         }
     }
 }
