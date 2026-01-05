4b413935aaf519a5e275a9e24a83212e4032d04a
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2026-01-05 14:20:26 +0100

    0501261420

---
+++ Les12/Bestelling/Bestelling/FrmBestelling.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Bestelling
+{
+    partial class FrmBestelling
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmBestelling));
+            this.btnNewOrder = new System.Windows.Forms.Button();
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lbxOrder = new System.Windows.Forms.ListBox();
+            this.btnSummary = new System.Windows.Forms.Button();
+            this.txtSummary = new System.Windows.Forms.TextBox();
+            this.cbxMenu = new System.Windows.Forms.ComboBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.printPreviewDialog = new System.Windows.Forms.PrintPreviewDialog();
+            this.SuspendLayout();
+            // 
+            // btnNewOrder
+            // 
+            this.btnNewOrder.Location = new System.Drawing.Point(13, 13);
+            this.btnNewOrder.Name = "btnNewOrder";
+            this.btnNewOrder.Size = new System.Drawing.Size(246, 23);
+            this.btnNewOrder.TabIndex = 6;
+            this.btnNewOrder.Text = "Nieuwe bestelling";
+            this.btnNewOrder.UseVisualStyleBackColor = true;
+            this.btnNewOrder.Click += new System.EventHandler(this.BtnNewOrder_Click);
+            // 
+            // txtItem
+            // 
+            this.txtItem.Location = new System.Drawing.Point(13, 72);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(83, 20);
+            this.txtItem.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 98);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(83, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Toevoegen";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(13, 127);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(83, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Verwijderen";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // lbxOrder
+            // 
+            this.lbxOrder.FormattingEnabled = true;
+            this.lbxOrder.Location = new System.Drawing.Point(12, 156);
+            this.lbxOrder.Name = "lbxOrder";
+            this.lbxOrder.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lbxOrder.Size = new System.Drawing.Size(83, 134);
+            this.lbxOrder.Sorted = true;
+            this.lbxOrder.TabIndex = 5;
+            // 
+            // btnSummary
+            // 
+            this.btnSummary.Location = new System.Drawing.Point(102, 43);
+            this.btnSummary.Name = "btnSummary";
+            this.btnSummary.Size = new System.Drawing.Size(157, 23);
+            this.btnSummary.TabIndex = 3;
+            this.btnSummary.Text = "Overzicht bestelling";
+            this.btnSummary.UseVisualStyleBackColor = true;
+            this.btnSummary.Click += new System.EventHandler(this.BtnSummary_Click);
+            // 
+            // txtSummary
+            // 
+            this.txtSummary.Location = new System.Drawing.Point(101, 72);
+            this.txtSummary.Multiline = true;
+            this.txtSummary.Name = "txtSummary";
+            this.txtSummary.Size = new System.Drawing.Size(156, 218);
+            this.txtSummary.TabIndex = 4;
+            // 
+            // cbxMenu
+            // 
+            this.cbxMenu.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxMenu.FormattingEnabled = true;
+            this.cbxMenu.Location = new System.Drawing.Point(13, 44);
+            this.cbxMenu.Name = "cbxMenu";
+            this.cbxMenu.Size = new System.Drawing.Size(82, 21);
+            this.cbxMenu.TabIndex = 7;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(12, 297);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(245, 23);
+            this.btnPrint.TabIndex = 8;
+            this.btnPrint.Text = "Printen";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.BtnPrint_Click);
+            // 
+            // printPreviewDialog
+            // 
+            this.printPreviewDialog.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog.Enabled = true;
+            this.printPreviewDialog.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog.Icon")));
+            this.printPreviewDialog.Name = "printPreviewDialog";
+            this.printPreviewDialog.Visible = false;
+            // 
+            // FrmBestelling
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 323);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.cbxMenu);
+            this.Controls.Add(this.txtSummary);
+            this.Controls.Add(this.btnSummary);
+            this.Controls.Add(this.lbxOrder);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtItem);
+            this.Controls.Add(this.btnNewOrder);
+            this.Name = "FrmBestelling";
+            this.Text = "Bestelling";
+            this.Load += new System.EventHandler(this.FrmBestelling_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewOrder;
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ListBox lbxOrder;
+        private System.Windows.Forms.Button btnSummary;
+        private System.Windows.Forms.TextBox txtSummary;
+        private System.Windows.Forms.ComboBox cbxMenu;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog;
+    }
+}
+
---
+++ Les12/Bestelling/Bestelling/FrmBestelling.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bestelling
+{
+    public partial class FrmBestelling : Form
+    {
+        // globale list aanmaken
+        List<string> orderItems = new List<string>();
+
+        public FrmBestelling()
+        {
+            InitializeComponent();
+        }
+
+        #region methods
+
+        // methode om invoer uit txtItem
+        // toe te voegen aan list en listbox te updaten
+        private void AddItem(string item)
+        {
+            // invoer string in correcte formaat zetten
+            item = Capitalise(item);
+            // invoer uit txtItem toevoegen aan list
+            orderItems.Add(item);
+            // updateorder methode uitvoeren om in listbox te tonen
+            UpdateOrder();
+        }
+
+        // methode om listbox te updaten
+        private void UpdateOrder()
+        {
+            // listbox eerst weer leegmaken voor de update
+            lbxOrder.Items.Clear();
+
+            // met foreach alle items uit list tonen in listbox
+            foreach (var item in orderItems)
+            {
+                lbxOrder.Items.Add(item);
+            }
+        }
+
+        // methode om alle geselecteerde items uit de list te verwijderen
+        // en ook de listbox te updaten
+        private  void DeleteItems()
+        {
+            // alle geselecteerde items uit listbox
+            // verwijderen uit list
+            foreach (string item in lbxOrder.SelectedItems)
+            {
+                orderItems.Remove(item);
+            }
+
+            // listbox updaten
+            UpdateOrder();
+        }
+
+        // methode om een string te 'capitalisen'
+        // betekent: eerste letter hoofdletter, andere kleine letters
+        // en ook spaties voor en na de string wegnemen
+        private string Capitalise(string input)
+        {
+            // spaties wegnemen
+            string result = input.Trim();
+
+            // eerste letter ophalen
+            char firstLetter = result[0];
+            // rest van woord
+            string restOfWord = result.Substring(1);
+
+            // aan elkaar plakken in juiste case
+            result = char.ToUpper(firstLetter) 
+                    + restOfWord.ToLower();
+
+            return result;
+        }
+
+        #endregion
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            // controleren of textbox niet leeg is
+            // als dit OK is --> dan wel code uitvoeren, anders foutboodschap
+            if (txtItem.Text != "")
+            {
+                // add item methode uitvoeren om item toe te voegen
+                // aan zowel list als listbox
+                AddItem(txtItem.Text);
+
+                // input vak (txtItem) weer leeg maken voor volgende item
+                txtItem.Clear();
+            }
+            else
+            {
+                // geselecteerde item uit cbxMenu toevoegen aan list en listbox
+                AddItem(cbxMenu.Text);
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            DeleteItems();
+        }
+
+        private void FrmBestelling_Load(object sender, EventArgs e)
+        {
+            // vaste array met menu items maken
+            string[] menuItems = { "Koffie", "Thee", "Muffin" };
+            // alle elementen uit menuItems toevoegen aan cbxMenu
+            foreach (var item in menuItems)
+            {
+                cbxMenu.Items.Add(item);
+            }
+            // eerste element uit combobox al actief zetten (selecteren)
+            cbxMenu.SelectedIndex = 0;
+        }
+
+        private void BtnSummary_Click(object sender, EventArgs e)
+        {
+            // string aanmaken om output in te voorzien
+            string orderSummary = "";
+
+            // ieder element uit de list (orderItems)
+            // in string plaatsen telkens op een nieuwe regel
+            // Distinct om ieder soort maar één keer te hebben
+            foreach(var item in orderItems.Distinct())
+            {
+                // opslaan hoe veel keer een item in de
+                // volledige list voorkomt
+                int amount = orderItems.Count(x => x == item);
+                orderSummary += item + " \tx " + amount + "\r\n";
+            }
+
+            // string in textbox plaatsen en laatste lege regel verwijderen
+            txtSummary.Text = orderSummary.Trim();
+        }
+
+        private void BtnNewOrder_Click(object sender, EventArgs e)
+        {
+            // list orderItems leegmaken
+            orderItems.Clear();
+            // listbox opnieuw opbouwen met huidige inhoud list die nu leeg is
+            UpdateOrder();
+            // textbox txtSummary leeg maken
+            txtSummary.Clear();
+            // cursor in txtItem plaatsen
+            txtItem.Focus();
+        }
+
+        private void BtnPrint_Click(object sender, EventArgs e)
+        {
+            // maak nieuw document om te printen aan in de code
+            PrintDocument document = new PrintDocument();
+            // pagina om te printen toevoegen aan document
+            document.PrintPage += Document_PrintPage;
+            // grootte afdrukvoorbeeld aanpassen
+            printPreviewDialog.ClientSize = new Size(800,600);
+            // document correct instellen voor afdrukvoorbeeld
+            printPreviewDialog.Document = document;
+            // toon afdrukvoorbeeld
+            printPreviewDialog.ShowDialog();
+        }
+
+        // methode die printblad opbouwt
+        private void Document_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            // lettertype instellen voor tekst die moet geprint worden
+            Font printFont = new Font("Arial", 12);
+            // linkermarge koppelen aan variabele x
+            int x = e.MarginBounds.Left;
+            // bovenmarge koppelen aan variabele y
+            int y = e.MarginBounds.Top;
+
+            // tekst die moet geprint worden in variabele opslaan
+            string textToPrint = "Bestelling: " + "\r\n\r\n" + txtSummary.Text;
+
+            // tekst 'tekenen' op blad
+            e.Graphics.DrawString(textToPrint, printFont, Brushes.Black, x, y); 
+        }
+    }
+}
