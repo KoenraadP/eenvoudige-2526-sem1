e5fede7207f899ff38c5b31c399b6b0107ffbaad
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2026-01-12 13:23:04 +0100

    120123

---
+++ Les14/herhaling_classes/herhaling_classes/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace herhaling_classes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxCars = new System.Windows.Forms.ComboBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cbxCars
+            // 
+            this.cbxCars.FormattingEnabled = true;
+            this.cbxCars.Location = new System.Drawing.Point(13, 13);
+            this.cbxCars.Name = "cbxCars";
+            this.cbxCars.Size = new System.Drawing.Size(121, 21);
+            this.cbxCars.TabIndex = 0;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(13, 44);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(121, 20);
+            this.txtPrice.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(149, 76);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.cbxCars);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxCars;
+        private System.Windows.Forms.TextBox txtPrice;
+    }
+}
+
